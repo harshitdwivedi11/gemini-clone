012eb72c1fafb1a268728338e15f3431f39a7f37
harshitdwivedi11 <141849977+harshitdwivedi11@users.noreply.github.com>
2025-02-22 22:03:15 +0530

    text generate effect added

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -24,6 +24,7 @@ const chat = model.startChat({
   const result = await chat.sendMessage(prompt);
   const response =result.response;
   console.log(response.text());
+  return response.text();
 }
 
 export default run;
